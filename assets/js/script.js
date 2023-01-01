310ff6d7d686048bf880d7692694199adc05f450
ms rashid <msrashidgb@gmail.com>
2023-01-01 16:53:55 +0000

    Setting current date in js file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,12 @@ function scheduler() {
 
   $(document).ready(function () {
     var currentDay = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+    $("#currentDay").text(currentDay);
+
+    var hours = ["9 AM.", "10 AM.", "11 AM.", "12 PM.", "1 PM.", "2 PM.", "3 PM.", "4 PM.",
+      "5 PM."];
+    var k=9;
   }
+}
  
